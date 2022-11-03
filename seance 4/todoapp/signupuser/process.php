b9d47917bed3b51eecda96cfa6386588be3e97b3
hkimi amin <hkimiamin02@gmail.com>
2022-11-03 20:56:49 +0100

    fix sign up

--- seance 4/todoapp/signupuser/process.php
+++ seance 4/todoapp/signupuser/process.php
@@ -4,47 +4,50 @@ $error=[];
 if(isset($_POST['submit'])){
     extract($_POST);
     if($username=="" || $email=="" || $password=="" || $password2==""){
-        $error[0]="all form feilds should be feild";
-        goto show;
+        header("location:process.php?msg=all form feilds should be feild&type=danger");
+        exit;
     }
     $req=$db->prepare('SELECT * FROM users where email=:email');
     $req->execute(['email'=>$email]);
     if($req->rowCount()){
-        $error[0]="enter a not excisting email";
-        goto show;
+        header("location:process.php?msg=enter a not excisting email&type=danger");
+        exit;   
     }
     else{
-        // $name_file= $_POST['avatar']['name'];
-        // $type=pathinfo($name_file,PATHINFO_EXTENSION);
-        // $type_dispo=['png','jpg','jpeg','gif'];
-        // if(!in_array($type,$type_dispo)){
-        //     $error[0]="extantion invalid";
-        //     goto show;
-        // }
-        // $size=$_FILES['avatar']['size'];
-        // if($size>999999999){
-        //     $error[0]="image size too large";
-        //     goto show;
-        // }
-        // $name_file=md5(mt_rand()).'.'.$type;
-        // $avatar='./storage/'.$name_file;
-        // if(!move_uploaded_file($_FILES['avatar']['tmp_name'],'./storage/'.$name_file)){
-        //     $error[0]="image not uploaded";
-        //     goto show;
-        // }
+        if(!strlen($_FILES['avatar']['name'])){
+            header("location:process.php?msg=file required&type=danger");
+            exit;
+        }
+     $name_file= $_FILES['avatar']['name'];
+     $type=pathinfo($name_file,PATHINFO_EXTENSION);
+        $type_dispo=['png','jpg','jpeg','gif'];
+        if(!in_array($type,$type_dispo)){
+            header("location:process.php?msg=extention invalid&type=danger");
+            exit;
+        }
+        $size=$_FILES['avatar']['size'];
+        if($size>999999999){
+            header("location:process.php?msg=image size too large&type=danger");
+            exit;
+        }
+        $name_file=md5(mt_rand()).'.'.$type;
+        
+        if(!move_uploaded_file($_FILES['avatar']['tmp_name'],'../storage/'.$name_file)){
+            header("location:process.php?msg=image not uploaded&type=danger");
+            exit;
+        }
+        $avatar='../storage/'.$name_file;
         if($password!=$password2){
-            $error[0]="passwords dont match";
-            goto show;
+            header("location:process.php?msg=passwords dont match&type=danger");
+            exit;
         }
-        else{
-            $password=password_hash($password,PASSWORD_DEFAULT);
-        }
-            $req=$db->prepare("INSERT INTO users (username,email,password) VALUES(:username,:email,:password)");
+            $req=$db->prepare("INSERT INTO users (username,email,password,avatar) VALUES(:username,:email,:password,:file)");
             $req->execute(['username'=>$username,
                             'email'=>$email,
-                            'password'=>$password,
+                            'password'=>password_hash($password,PASSWORD_DEFAULT),
+                            'file'=>$avatar
         ]);
-        header("location:home.phtml?msg=user saved you may now log in");
+        header("location:process.php?msg=user saved you may now log in&type=success");  
     }
 }
 show:
