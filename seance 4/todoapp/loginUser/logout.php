c301d77207272bf43fb067fcfe0ed93e082e445f
hkimi amin <hkimiamin02@gmail.com>
2022-11-06 12:37:40 +0100

    remove todo

--- seance 4/todoapp/loginUser/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    session_start();
-    session_destroy();
-    unset($_SESSION['name']);
-    header("location:loginPage.phtml?msg=you have been loged out&type=success");
-?>
--- seance 4/todoapp/script/script.js
+++
@@ -1,6 +0,0 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-    myInput.focus()
-})
