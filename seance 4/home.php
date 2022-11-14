75e85ceced9f9cfc36f4ff88bdbeff5188a94a9a
hkimi amin <hkimiamin02@gmail.com>
2022-11-14 11:43:41 +0100

    update

--- seance 4/home.php
+++ seance 4/home.php
@@ -16,8 +16,8 @@
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" name="password">
   </div>
   <button type="submit" class="btn btn-primary" name="submit">Submit</button>
 </form>
