5eccfe1db13602b7675d823fde8d9d4c110d90f0
hkimi amin <96321002+hkimi02@users.noreply.github.com>
2022-11-15 13:32:21 +0100

    update

--- seance 4/home.php
+++ seance 4/home.php
@@ -13,7 +13,6 @@
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="password" class="form-label">Password</label>
